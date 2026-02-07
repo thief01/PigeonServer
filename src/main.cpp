64909aa2b65ab6415b3e1bc958588c30b2e33090
thief01 <pst12345@wp.pl>
2026-02-07 10:20:05 +0100

    add config

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <thread>
-#include "ServerCore.h"
+#include "Core/ServerCore.h"
 
 int main() {
     asio::io_context io;
-
-    Server server(io, 12345);
+    Config config = Config("config.ini");
+    Server server(io, config);
 
     // Asio async w osobnym wątku
     std::thread asioThread([&io]() { io.run(); });
@@ -12,7 +12,7 @@ int main() {
     // Tick loop ~14ms (70 FPS)
     while (true) {
         server.tick();
-        std::this_thread::sleep_for(std::chrono::milliseconds(14));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / config.tickRate));
     }
 
     asioThread.join();
