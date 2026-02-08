81edc6052c785c17476b7937241c6dbf06fb5c9c
thief01 <pst12345@wp.pl>
2026-02-08 07:52:51 +0100

    remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,8 @@ int main() {
     Config config = Config("config.ini");
     TcpServer server(io, config);
 
-
-
-    // Asio async w osobnym wątku
     std::thread asioThread([&io]() { io.run(); });
 
-    // Tick loop ~14ms (70 FPS)
     while (true) {
         server.tick();
         std::this_thread::sleep_for(std::chrono::milliseconds(1000 / config.tickRate));
