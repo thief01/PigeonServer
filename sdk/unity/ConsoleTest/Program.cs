92effe8e20ab0c86ebe795ab9960fcd9d9aac077
thief01 <pst12345@wp.pl>
2026-02-11 10:42:48 +0100

    add client and console projects

---
+++ sdk/unity/ConsoleTest/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Client.Client client = new Client.Client();
+
+client.Connect("127.0.0.1", 12345);
---
+++ sdk/unity/client/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public class Client
+    {
+        public Action OnConnected;
+        public TcpClient TcpClient { get; private set; }
+
+        public Client()
+        {
+            TcpClient = new TcpClient();
+        }
+
+        public void Connect(string host, int port)
+        {
+            TcpClient.Connect(host, port);
+        }
+    }
+}
