12ba007122046ea1f04e48c65e47dd2c032d6349
thief01 <pst12345@wp.pl>
2026-02-23 14:48:59 +0100

    create class for test application

---
+++ sdk/unity/ConsoleTest/PigeonApp.cs
@@ -0,0 +1,38 @@
+﻿using Client;
+
+namespace ConsoleTest;
+
+public class PigeonApp
+{
+    private PigeonClient pigeonClient;
+
+    public PigeonApp()
+    {
+        pigeonClient = new PigeonClient(new ClientOptions()
+        {
+            Logger = new Logger(),
+            Host = "127.0.0.1",
+            Port = 12345,
+        });
+    }
+
+    public void Run()
+    {
+        pigeonClient.ConnectAsync();
+        while (pigeonClient.Connected)
+        {
+            
+        }
+    }
+
+    private void SendData()
+    {
+        var data = Console.ReadLine();
+        
+    }
+
+    private void ReceiveDatA()
+    {
+        
+    }
+}
